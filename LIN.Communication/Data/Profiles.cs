b1dc0096cbaf8060e35375177201b11a26aa6238
Alex <giraldojhong4@gmail.com>
2023-08-26 13:05:35 -0500

    Profile Data

---
+++ LIN.Communication/Data/Profiles.cs
@@ -0,0 +1,56 @@
+﻿namespace LIN.Communication.Data;
+
+
+public class Profiles
+{
+
+
+
+    #region Abstracciones
+
+
+    public async static Task<CreateResponse> Create(ProfileModel data)
+    {
+
+        // Contexto
+        (Conexión context, string connectionKey) = Conexión.GetOneConnection();
+
+        // respuesta
+        var response = await Create(data, context);
+
+        context.CloseActions(connectionKey);
+
+        return response;
+
+    }
+
+
+
+
+
+    #endregion
+
+
+
+    public async static Task<CreateResponse> Create(ProfileModel data, Conexión context)
+    {
+        // ID
+        data.ID = 0;
+
+        // Ejecución
+        try
+        {
+            var res = context.DataBase.Profiles.Add(data);
+            await context.DataBase.SaveChangesAsync();
+            return new(Responses.Success, data.ID);
+        }
+        catch 
+        {
+        }
+        return new();
+    }
+
+
+
+
+}
