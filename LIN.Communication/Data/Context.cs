92915aad65e1de1adc87f104602de08b4d129457
Alex <giraldojhong4@gmail.com>
2023-08-26 22:01:08 -0500

    Correccion del Context DB

--- LIN.Communication/Data/Context.cs
+++ LIN.Communication/Data/Context.cs
@@ -1,18 +1,25 @@
-﻿using LIN.Types.Auth.Models;
-
-namespace LIN.Communication.Data;
+﻿namespace LIN.Communication.Data;
 
 
 public class Context : DbContext
 {
 
 
-
+    /// <summary>
+    /// Perfiles
+    /// </summary>
     public DbSet<ProfileModel> Profiles { get; set; }
 
 
-
+    /// <summary>
+    /// Conversaciones
+    /// </summary>
     public DbSet<ConversationModel> Conversaciones { get; set; }
+
+
+
+   
+    public DbSet<MemberChatModel> Member { get; set; }
 
 
 
@@ -34,31 +41,7 @@ public class Context : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
 
-        // Indices y identidad
-        modelBuilder.Entity<ProfileModel>()
-           .HasIndex(e => e.Alias)
-           .IsUnique();
-
-
-        modelBuilder.Entity<ConversationModel>()
-          .HasOne(p => p.UsuarioA)
-          .WithMany()
-          .HasForeignKey(p => p.UsuarioAID)
-          .OnDelete(DeleteBehavior.Cascade);
-
-        modelBuilder.Entity<ConversationModel>()
-         .HasOne(p => p.UsuarioB)
-         .WithMany()
-         .HasForeignKey(p => p.UsuarioBID)
-         .OnDelete(DeleteBehavior.NoAction);
-
-
-        modelBuilder.Entity<ConversationModel>()
-           .HasKey(a => a.ID);
-
-
-        modelBuilder.Entity<ConversationModel>()
-            .HasKey(a => new { a.ID, a.UsuarioAID, a.UsuarioBID });
+      
 
     }
 
