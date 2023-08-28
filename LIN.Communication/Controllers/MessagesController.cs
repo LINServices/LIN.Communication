c5d1d322fc86416d98cd2ce1a2dbc85cdd1242cf
Alex <giraldojhong4@gmail.com>
2023-08-28 09:26:01 -0500

    Messages Controller

---
+++ LIN.Communication/Controllers/MessagesController.cs
@@ -0,0 +1,25 @@
+﻿namespace LIN.Communication.Controllers;
+
+
+[Route("conversations")]
+public class MessagesController : ControllerBase
+{
+
+
+    /// <summary>
+    /// Obtiene la lista de mensajes asociados a una conversación.
+    /// </summary>
+    /// <param name="conversation">ID de la conversación</param>
+    [HttpGet("{id}/messages")]
+    public async Task<HttpReadAllResponse<MessageModel>> ReadAll([FromRoute] int conversation)
+    {
+        // Obtiene el usuario
+        var result = await Data.Messages.ReadAll(conversation);
+
+        // Retorna el resultado
+        return result ?? new();
+
+    }
+
+
+}
