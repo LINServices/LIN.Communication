c6028e3c0e3b6d04fe302b0db717b64760ef4fc3
Alex <giraldojhong4@gmail.com>
2023-09-04 13:49:13 -0500

    Controlador de Members

---
+++ LIN.Communication/Controllers/MembersController.cs
@@ -0,0 +1,26 @@
+﻿namespace LIN.Communication.Controllers;
+
+
+[Route("conversations")]
+public class MembersController : ControllerBase
+{
+
+
+    /// <summary>
+    /// Obtiene los miembros de una conversación
+    /// </summary>
+    /// <param name="id">ID de la conversación.</param>
+    [HttpGet("{id}/members")]
+    public async Task<HttpReadAllResponse<MemberChatModel>> ReadAll([FromRoute] int id)
+    {
+
+        // Obtiene el usuario
+        var result = await Data.Conversations.ReadMembers(id);
+
+        // Retorna el resultado
+        return result ?? new();
+
+    }
+
+
+}
