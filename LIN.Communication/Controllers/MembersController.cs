b539574326b8a8cadb5cfc8b16f5378e70e9ad13
Alex <giraldojhong4@gmail.com>
2023-09-23 15:26:45 -0500

    Readme.MD

--- LIN.Communication/Controllers/MembersController.cs
+++ LIN.Communication/Controllers/MembersController.cs
@@ -23,4 +23,43 @@ public class MembersController : ControllerBase
     }
 
 
+
+
+    /// <summary>
+    /// Obtiene los miembros de una conversación
+    /// </summary>
+    /// <param name="id">ID de la conversación.</param>
+    [HttpGet("{id}/members/info")]
+    public async Task<HttpReadAllResponse<SessionModel<ProfileModel>>> ReadAllInfo([FromRoute] int id)
+    {
+
+        // Obtiene el usuario
+        var result = await Data.Conversations.ReadMembers(id);
+
+        var x = result.Models.Select(T => T.Profile.AccountID).ToList();
+
+        var resultAccounts = await LIN.Access.Auth.Controllers.Account.Read(x);
+
+
+
+        var re = (from P in result.Models
+                 join A in resultAccounts.Models
+                 on P.Profile.AccountID equals A.ID
+                 select new SessionModel<ProfileModel>
+                 {
+                     Account = A,
+                     Profile = P.Profile
+                 }).ToList();
+
+
+        // Retorna el resultado
+        return new ReadAllResponse<SessionModel<ProfileModel>>
+        {
+            Models = re,
+            Response = Responses.Success
+        };
+
+    }
+
+
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+# LIN Communication Services
+
+LIN Communication Services es un servicio de comunicación en tiempo real que utiliza SignalR para el envío de mensajes y almacena los mensajes en una base de datos SQL Server utilizando Entity Framework Core. Esta plataforma permite la comunicación en tiempo real y el registro de mensajes para su posterior análisis.
+
+## Requisitos
+
+- .NET Core SDK (versión 3.1 o superior)
+- SQL Server instalado y configurado
+- Conexión a internet (para la comunicación en tiempo real)
