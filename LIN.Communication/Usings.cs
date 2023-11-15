eb7453901c563a1ca13f502cfd9d4a1956daadf8
Alex <giraldojhong4@gmail.com>
2023-11-14 23:54:14 -0500

    Usings

---
+++ LIN.Communication/Usings.cs
@@ -0,0 +1,12 @@
+﻿global using Http.ResponsesList;
+global using LIN.Communication;
+global using LIN.Communication.Memory;
+global using LIN.Communication.Services;
+global using LIN.Modules;
+global using LIN.Types.Auth.Abstracts;
+global using LIN.Types.Auth.Enumerations;
+global using LIN.Types.Communication.Models;
+global using LIN.Types.Responses;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.SignalR;
+global using Microsoft.EntityFrameworkCore;
