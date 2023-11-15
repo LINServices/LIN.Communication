c47bb539aa2961cc5bde6861040fd47d78a9104d
Alex <giraldojhong4@gmail.com>
2023-11-14 22:59:23 -0500

    Mejoras de seguridad en controlador de Conversaciones

--- LIN.Communication/Controllers/ConversationController.cs
+++ LIN.Communication/Controllers/ConversationController.cs
@@ -1,5 +1,3 @@
-using LIN.Communication.Memory;
-
 namespace LIN.Communication.Controllers;
 
 
@@ -9,9 +7,54 @@ public class ConversationController : ControllerBase
 
 
 
+    /// <summary>
+    /// Crear nueva conversación.
+    /// </summary>
+    /// <param name="modelo">Modelo.</param>
+    /// <param name="token">Token de acceso.</param>
     [HttpPost("create")]
-    public async Task<HttpCreateResponse> Create([FromBody] ConversationModel modelo)
+    public async Task<HttpCreateResponse> Create([FromBody] ConversationModel modelo, [FromHeader] string token)
     {
+
+        // Información del token.
+        var (isValid, profileID, _, _) = Jwt.Validate(token);
+
+        // Valida el token.
+        if (isValid)
+            return new()
+            {
+                Message = "El token es invalido.",
+                Response = Responses.Unauthorized
+            };
+
+        // Organizar el modelo.
+        modelo.ID = 0;
+        modelo.Mensajes = new();
+        modelo.Members ??= new();
+
+        // Integrantes.
+        List<MemberChatModel> members = new();
+        foreach(var member in modelo.Members)
+        {
+            member.ID = 0;
+            if (member.Profile.ID == profileID)
+                continue;
+
+            members.Add(member);
+        }
+
+        // Agrega al administrador.
+        members.Add(new()
+        {
+            Profile = new()
+            {
+                ID = profileID
+            },
+            Rol = Types.Communication.Enumerations.MemberRoles.Admin
+        });
+
+        // Establecer los miembros.
+        modelo.Members = members;
 
         // Obtiene el resultado
         var response = await Data.Conversations.Create(modelo);
@@ -23,19 +66,25 @@ public class ConversationController : ControllerBase
 
 
 
+    /// <summary>
+    /// Obtiene las conversaciones asociadas a un perfil.
+    /// </summary>
+    /// <param name="token">Token de acceso.</param>
     [HttpGet("read/all")]
     public async Task<HttpReadAllResponse<MemberChatModel>> ReadAll([FromHeader] string token)
     {
 
-        // Comprobaciones
+        // Información del token.
         var (isValid, profileID, _, _) = Jwt.Validate(token);
 
+        // Si el token es invalido.
         if (!isValid)
             return new(Responses.Unauthorized);
 
-        // Obtiene el usuario
+        // Obtiene el usuario.
         var result = await Data.Conversations.ReadAll(profileID);
 
+        // Sesión en memoria.
         var onHub = Mems.Sessions[profileID];
         if (onHub != null)
         {
@@ -51,8 +100,13 @@ public class ConversationController : ControllerBase
 
 
 
+    /// <summary>
+    /// Obtiene una conversación.
+    /// </summary>
+    /// <param name="id">Id de la conversación.</param>
+    /// <param name="token">Token de acceso.</param>
     [HttpGet("read/one")]
-    public async Task<HttpReadOneResponse<ConversationModel>> ReadOne([FromQuery] int id)
+    public async Task<HttpReadOneResponse<ConversationModel>> ReadOne([FromQuery] int id, [FromHeader] string token)
     {
 
         // Obtiene el usuario
