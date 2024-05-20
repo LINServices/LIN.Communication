d941ed0b2217bbf39a92651d1837224739ae9917
Alex <giraldojhong4@gmail.com>
2024-05-20 17:20:07 -0500

    Endpoint para enviar mensaje

--- LIN.Communication/Controllers/MessagesController.cs
+++ LIN.Communication/Controllers/MessagesController.cs
@@ -1,11 +1,12 @@
-﻿using LIN.Communication.Services.Iam;
+﻿using LIN.Communication.Hubs;
+using LIN.Communication.Services.Iam;
 using LIN.Communication.Services.Models;
 
 namespace LIN.Communication.Controllers;
 
 
 [Route("conversations")]
-public class MessagesController(IIamService Iam) : ControllerBase
+public class MessagesController(IIamService Iam, IHubContext<ChatHub> hub) : ControllerBase
 {
 
 
@@ -42,5 +43,77 @@ public class MessagesController(IIamService Iam) : ControllerBase
 
     }
 
+
+
+
+    /// <summary>
+    /// Enviar un mensaje.
+    /// </summary>
+    /// <param name="token">Token de acceso.</param>
+    /// <param name="id">Id de la conversación.</param>
+    /// <param name="message">Contenido del mensaje.</param>
+    /// <param name="guid">Guid único</param>
+    [HttpPost("{id:int}/messages")]
+    [LocalToken]
+    public async Task<HttpCreateResponse> Post([FromHeader] string token, [FromRoute] int id, [FromBody] string message, [FromQuery] string guid)
+    {
+
+        // Validar contenido.
+        if (string.IsNullOrWhiteSpace(message))
+            return new()
+            {
+                Message = "Message content can´t be empty",
+                Response = Responses.InvalidParam,
+            };
+
+        // Información del token.
+        JwtModel tokenInfo = HttpContext.Items[token] as JwtModel ?? new();
+
+        // Valida el acceso Iam.
+        var iam = await Iam.Validate(tokenInfo.ProfileId, id);
+
+        // Valida el acceso Iam.
+        if (iam == IamLevels.NotAccess)
+            return new()
+            {
+                Response = Responses.Unauthorized,
+                Message = "No tienes acceso a esta conversación."
+            };
+
+
+        // Hora actual.
+        var time = DateTime.Now;
+
+        // Modelo del mensaje.
+        MessageModel messageModel = new()
+        {
+            Contenido = message,
+            Remitente = new()
+            {
+                AccountID = tokenInfo.AccountId,
+                Alias = tokenInfo.Alias,
+                ID = tokenInfo.ProfileId
+            },
+            Time = new DateTime(time.Year, time.Month, time.Day, time.Hour, time.Minute, 0),
+            Guid = guid,
+            Conversacion = new()
+            {
+                ID = id
+            }
+        };
+
+        // Envía el mensaje en tiempo real.
+        await hub.Clients.Group(id.ToString()).SendAsync($"sendMessage", messageModel);
+
+        // Crea el mensaje en la BD
+        await Data.Messages.Create(messageModel);
+
+        // Retorna el resultado.
+        return new()
+        {
+            Response = Responses.Success
+        };
+
+    }
 
 }
